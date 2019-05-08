e0132a43bc6d9a3f7f5e47c8e75330ad08351a06
senyast4745 <senyasdt4745@gmail.com>
2019-05-09 00:14:35 +0300

    A Time Commit

--- src/main/java/ru/mail/polis/senyast4745/Value.java
+++ src/main/java/ru/mail/polis/senyast4745/Value.java
@@ -3,10 +3,14 @@ package ru.mail.polis.senyast4745;
 import org.jetbrains.annotations.NotNull;
 
 import java.nio.ByteBuffer;
+import java.sql.Time;
+import java.util.Timer;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public final class Value implements Comparable<Value> {
     private final long ts;
     private final ByteBuffer data;
+    private static final AtomicInteger atomicInteger = new AtomicInteger();
 
     private Value(final long ts, final ByteBuffer data) {
         assert ts >= 0;
@@ -15,7 +19,7 @@ public final class Value implements Comparable<Value> {
     }
 
     public static Value of(final ByteBuffer data) {
-        return new Value(System.currentTimeMillis(), data.duplicate());
+        return new Value(getTime(), data.duplicate());
     }
 
     public static Value of(final long time, final ByteBuffer data) {
@@ -48,5 +52,11 @@ public final class Value implements Comparable<Value> {
 
     long getTimeStamp() {
         return ts;
+    }
+
+    private static long getTime() {
+        final long time = System.currentTimeMillis() * 1000 + atomicInteger.incrementAndGet();
+        if (atomicInteger.get() > 1000) atomicInteger.set(0);
+        return time;
     }
 }
